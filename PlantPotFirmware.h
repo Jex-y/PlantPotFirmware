e86aebcc2ad0fc8d37889ea0ace93a1bc1163cfe
Edward Jex <edwardjex@live.co.uk>
2020-05-17 22:21:38 +0100

    Firmware base complete, network time functions removed as there is not enough program space, may be added back later.

--- PlantPotFirmware.h
+++ PlantPotFirmware.h
@@ -1,22 +1,16 @@
 #pragma once
 void setup();
 void loop();
-void tasksInit();
-void task0_1(void* params);
-void task0_2(void* params);
-void task1_1(void* params);
-void task1_2(void* params);
 void pixelsInit();
-void tryWifiInit();
+//void tryWifiInit();
 void bleInit();
 void pinsInit();
 void updateMoisture();
 void updatePixels();
-void updateNetworkTime();
-unsigned long getTime();
-void updateBLE();
-char* floatToString(float value);
-char* boolToString(bool value);
+//void updateNetworkTime();
+//unsigned long getTime();
+void floatToString(char* buffer, float value);
+void boolToString(char* buffer, bool value);
 class ServerCallback : public BLEServerCallbacks {
 	void onConnect(BLEServer* pServer);
 	void onDisconect(BLEServer* pServer);
--- PlantPotFirmware.ino
+++ PlantPotFirmware.ino
@@ -1,9 +1,9 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-#include <WiFi.h>
-#include <NTPClient.h>
-#include <WiFIUdp.h>
+//#include <WiFi.h>
+//#include <NTPClient.h>
+//#include <WiFIUdp.h>
 #include <Adafruit_NeoPixel.h>
 #include <SPI.h>
 #include "UUIDs.h"
@@ -19,26 +19,27 @@
 Adafruit_NeoPixel strips[2];
 
 BLEServer* bleServer;
-bool* bleconnected = false;
+bool bleconnected = false;
 
-float* moisture;
+float moisture = 0;
 BLEService* MoistureService;
 BLECharacteristic* MoistureCharacteristic;
 
-float* threshold;
+float threshold = 0.5;
 BLEService* ThresholdService;
 BLECharacteristic* ThresholdCharacteristic;
 
-unsigned char colour[3];
+unsigned char colour[3] = {255, 0, 128};
 BLEService* ColourService;
 BLECharacteristic* RedCharacteristic;
 BLECharacteristic* GreenCharacteristic;
 BLECharacteristic* BlueCharacteristic;
 
-bool* lightsOn;
+bool lightsOn = false;
 BLEService* LightsOnService;
 BLECharacteristic* LightsOnCharacteristic;
 
+/*
 char ssid[20] = "VM8280875";
 char pass[20] = "s3bkCttHnpbr";
 byte ip[4];
@@ -53,105 +54,24 @@ WiFiUDP ntpUDP;
 NTPClient ntpClient(ntpUDP);
 unsigned long* lastTimeGet;
 unsigned int* networktime;
+*/
 
 void setup()
 {
+	Serial.begin(115200);
 	pinsInit();         // Initialise the GPIO pins for the pixels, moisture power and data and status LED.
 	pixelsInit();       // Initialise the pixels.
-	tryWifiInit();      // Initialise the WiFi and NTP clients, may fail if WiFi not available.
-	updateMoisture();   // Update the moisture for the first time.
+	//tryWifiInit();      // Initialise the WiFi and NTP clients, may fail if WiFi not available.
 	bleInit();          // Initialise the bluetooth low energy server.
-	updateBLE();        // Update the bluetooth low energy server for the first time.
+	updateMoisture();   // Update the moisture for the first time.
+	moisture *= 2;
 	updatePixels();     // Update the pixels for the first time.
-
-	tasksInit();		// Start the tasks.
 }
 
 void loop()
 {
-	// Should Never get here
-	delay(1000);
-}
-
-void tasksInit()
-{
-	TaskHandle_t task0_1Handle, task0_2Handle, task1_1Handle, task1_2Handle;
-
-	xTaskCreatePinnedToCore(
-		task0_1,
-		"task0_1",
-		10000,
-		NULL,
-		1,
-		&task0_1Handle,
-		0);
-
-	xTaskCreatePinnedToCore(
-		task0_2,
-		"task0_2",
-		10000,
-		NULL,
-		2,
-		&task0_2Handle,
-		0);
-
-	xTaskCreatePinnedToCore(
-		task1_1,
-		"task1_1",
-		10000,
-		NULL,
-		1,
-		&task1_1Handle,
-		1);
-
-	xTaskCreatePinnedToCore(
-		task1_2,
-		"task1_2",
-		10000,
-		NULL,
-		2,
-		&task1_2Handle,
-		1);
-}
-
-// Run Communications on core 0, environemnt updates on core 1.
-
-// Core 0, Priority 1 | Update BLE every 0.1 seconds.s
-void task0_1(void* params) 
-{ 
-	for (;;) {
-		updateBLE();
-		delay(100);
-	}
-}
-
-// Core 0, Priority 2 | Update network time every 10 minutes.
-void task0_2(void* params)
-{
-	for (;;) {
-		if ((millis() - *lastTimeGet) >= (1000 * 60 * 10)) {
-			updateNetworkTime();
-		}
-		delay(10000); // Check every 10 seconds.
-	}
-}
-
-// Core 1, Priority 1 | Update the pixels every 0.1 seconds.
-void task1_1(void* params)
-{
-	for (;;) {
-		updatePixels();
-		delay(100);
-	}
-}
-
-// Core 1, Priority 2 | Update the moisture sensor reading every 10 seconds
-void task1_2(void* params)
-{
-	for (;;) {
-		updateMoisture();
-		delay(10000);
-	}
+	updateMoisture();
+	delay(10000);
 }
 
 void pixelsInit()
@@ -160,12 +80,10 @@ void pixelsInit()
 	strips[1] = Adafruit_NeoPixel(NUM_LEDS, L2, NEO_GRB + NEO_KHZ800);
 	for (byte i = 0; i < 2; i++) {
 		strips[i].begin();
-		strips[i].fill(strips[i].Color(colour[0], colour[1], colour[2]));
-		strips[i].show();
 	}
 	return;
 }
-
+/*
 void tryWifiInit()
 {
 	// Starts WiFi and initialises the NTP client
@@ -183,15 +101,12 @@ connected:
 
 	ntpClient.begin();
 }
-
+*/
 void bleInit()
 {
-	*threshold = 0.5f;
-	colour[0] = 255; colour[1] = 0; colour[2] = 128;
 	BLEDevice::init("SmartPotv2");
 	bleServer = BLEDevice::createServer();
 	bleServer->setCallbacks(new ServerCallback());
-
 	MoistureService = bleServer->createService(MOISTURE_SERVICE_UUID);
 	MoistureCharacteristic = MoistureService->createCharacteristic(
 		MOISTURE_CHARACTERISTIC_UUID,
@@ -223,6 +138,7 @@ void bleInit()
 		BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
 	LightsOnCharacteristic->setCallbacks(new CharacteristicCallback());
 
+	/*
 	WiFiCredentailsService = bleServer->createService(WIFICREDENTIALS_SERVICE_UUID);
 	SSIDCharacteristic = WiFiCredentailsService->createCharacteristic(
 		SSID_CHARACTERISTIC_UUID,
@@ -238,13 +154,15 @@ void bleInit()
 	ConnectedCharactersitic = WiFiCredentailsService->createCharacteristic(
 		CONNECTED_CHARACTERISTIC_UUID,
 		BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-
+	*/
 	uint8_t value;
 
-	value = (int)(*moisture * 100);
+	value = (int)(moisture * 100);
 	MoistureCharacteristic->setValue(&value, sizeof(value));
-
-	ThresholdCharacteristic->setValue(floatToString(*threshold));
+	char buffer[8];
+	floatToString(buffer, threshold);
+	ThresholdCharacteristic->setValue(buffer);
+	clearBuffer(buffer,8);
 
 	value = colour[0];
 	RedCharacteristic->setValue(&value, sizeof(value));
@@ -252,31 +170,37 @@ void bleInit()
 	RedCharacteristic->setValue(&value, sizeof(value));
 	value = colour[2];
 	RedCharacteristic->setValue(&value, sizeof(value));
-
-	LightsOnCharacteristic->setValue(boolToString(lightsOn));
-
+	boolToString(buffer, lightsOn);
+	LightsOnCharacteristic->setValue(buffer);
+	/*
 	SSIDCharacteristic->setValue(ssid);
 	char ipstr[17];
 	sprintf(ipstr, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
 	IPCharacteristic->setValue(ipstr);
 	ConnectedCharactersitic->setValue(boolToString(wificonnected));
-
+	*/
 	MoistureService->start();
 	ThresholdService->start();
 	ColourService->start();
 	LightsOnService->start();
-	WiFiCredentailsService->start();
+	//WiFiCredentailsService->start();
 
 	BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
 	pAdvertising->addServiceUUID(MOISTURE_SERVICE_UUID);
 	pAdvertising->addServiceUUID(THRESHOLD_SERVICE_UUID);
 	pAdvertising->addServiceUUID(COLOUR_SERVICE_UUID);
 	pAdvertising->addServiceUUID(LIGHTSON_SERVICE_UUID);
-	pAdvertising->addServiceUUID(WIFICREDENTIALS_SERVICE_UUID);
+	//pAdvertising->addServiceUUID(WIFICREDENTIALS_SERVICE_UUID);
 	pAdvertising->setScanResponse(true);
 	pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
 	pAdvertising->setMinPreferred(0x12);
 	BLEDevice::startAdvertising();
+	MoistureCharacteristic->notify();
+	ThresholdCharacteristic->notify();
+	RedCharacteristic->notify();
+	GreenCharacteristic->notify();
+	BlueCharacteristic->notify();
+	LightsOnCharacteristic->notify();
 	return;
 }
 
@@ -294,14 +218,20 @@ void pinsInit()
 
 void updateMoisture()
 {
-	
+	Serial.write("updateMoisture called\n");
 	digitalWrite(moisturePower, HIGH);
 	digitalWrite(LED, HIGH);
+	Serial.write("Pins set to high\n");
 	delay(100);
-	float currentReading = ((float)analogRead(moistureData) / 4096);
-	*moisture = (currentReading + *moisture) / 2;
+	moisture = ((((float)analogRead(moistureData)) / 4096) + moisture)/2;
+	Serial.printf("Read value %f\n",moisture);
 	digitalWrite(moisturePower, LOW);
 	digitalWrite(LED, LOW);
+	Serial.write("Finished update moisture\n");
+	char buffer[8];
+	sprintf(buffer, "%1.4f", moisture);
+	MoistureCharacteristic->setValue(buffer);
+	MoistureCharacteristic->notify();
 	return;
 }
 
@@ -309,25 +239,24 @@ void updatePixels()
 {
 	for (byte i = 0; i < 2; i++) {
 
-		if (*lightsOn) {
+		if (lightsOn) {
 			strips[i].fill(strips[i].Color(colour[0], colour[1], colour[2]));
-			if (*moisture < *threshold)
+			if (moisture < threshold)
 			{
-				float multiplier = (*moisture) / (*threshold);
-				unsigned char b = (int)(multiplier * 255);
+				unsigned char b = (int)((moisture / threshold) * 255);
 				unsigned char g = 255 - b;
-				strips[i].setPixelColor(0, 0, g, b);
-				strips[i].setPixelColor(NUM_LEDS - 1, 0, g, b);
+				//strips[i].setPixelColor(0, 0, g, b);
+				//strips[i].setPixelColor(NUM_LEDS - 1, 0, g, b);
 			}
 		}
 		else {
-			strips[i].clear();
+			strips[i].fill(strips[i].Color(0, 0, 0));
 		}
 		strips[i].show();
 
 	}
 }
-
+/*
 void updateNetworkTime()
 {
 	 // Every 10 minutes
@@ -337,68 +266,78 @@ void updateNetworkTime()
 		}
 		*networktime = (int)(ntpClient.getHours() * 3600) + (int)(ntpClient.getMinutes() * 60) + (int)ntpClient.getSeconds();
 	}
-	*lastTimeGet = millis();
+	lastTimeGet = millis();
 	return;
 }
 
 unsigned long getTime() {
-	return *networktime + (int)((millis()-*lastTimeGet)/1000);
+	return networktime + (int)((millis()-lastTimeGet)/1000);
+}
+*/
+
+void floatToString(char* buffer,float value)
+{
+	Serial.printf("\t| Float to string called on value %f\n", value);
+	sprintf(buffer, "%.4f", value);
+	Serial.printf("\t| Returned a string: %s\n",buffer);
+	return;
 }
 
-void updateBLE()
+void boolToString(char* buffer, bool value)
 {
-	if (bleconnected) {
-		uint8_t value = ((int)(*moisture * 100));
-		MoistureCharacteristic->setValue(&value, sizeof(value));
-		MoistureCharacteristic->notify();
+	if (value) { buffer = "true"; }
+	else { buffer = "false"; }
+}
+
+void clearBuffer(char* buffer, byte length)
+{
+	for (byte i = 0; i < length; i++) {
+		buffer[i] = 0;
 	}
-}
-
-char* floatToString(float value)
-{
-	char result[16];
-	sprintf(result, "%.14f", value);
-	return result;
-}
-
-char* boolToString(bool value)
-{
-	char* resultptr = nullptr;
-	if (value) { resultptr = "true"; }
-	else { resultptr = "false"; }
 }
 
 void ServerCallback::onConnect(BLEServer* pServer)
 {
-	*bleconnected = true;
+	bleconnected = true;
 }
 
 void ServerCallback::onDisconect(BLEServer* pServer)
 {
-	*bleconnected = false;
+	bleconnected = false;
 }
 
 void CharacteristicCallback::onWrite(BLECharacteristic* pCharactersitic) {
 	BLEUUID targetUUID = pCharactersitic->getUUID();
+	Serial.write("Write callback called\n");
 	if (targetUUID.equals(ThresholdCharacteristic->getUUID())) {
-		*threshold = ((String)(char*)(pCharactersitic->getData())).toFloat();
+		threshold = ((String)(char*)(pCharactersitic->getData())).toFloat();
+		updatePixels();
 	}
 	else if (targetUUID.equals(RedCharacteristic->getUUID())) {
-		colour[0] = (unsigned char)pCharactersitic->getData();
+		colour[0] = (unsigned char)(*(char*)pCharactersitic->getData());
+		updatePixels();
 	}
 	else if (targetUUID.equals(GreenCharacteristic->getUUID())) {
-		colour[2] = (unsigned char)pCharactersitic->getData();
+		colour[1] = (unsigned char)(*(char*)pCharactersitic->getData());
+		updatePixels();
 	}
 	else if (targetUUID.equals(BlueCharacteristic->getUUID())) {
-		colour[3] = (unsigned char)pCharactersitic->getData();
+		colour[2] = (unsigned char)(*(char*)pCharactersitic->getData());
+		updatePixels();
 	}
 	else if (targetUUID.equals(LightsOnCharacteristic->getUUID())) {
-		*lightsOn = ((String)(char*)pCharactersitic->getData()).equals("true");
+		lightsOn = ((String)(char*)pCharactersitic->getData()).equals("true");
+		updatePixels();
 	}
+	else {
+		Serial.write("Target UUID didn't match anything\n");
+	}
+	/*
 	else if (targetUUID.equals(SSIDCharacteristic->getUUID())) {
-		((String)(char*)pCharactersitic->getData()).toCharArray(ssid, 20, 0);
+		//((String)(char*)pCharactersitic->getData()).toCharArray(ssid, 20, 0);
 	}
 	else if (targetUUID.equals(PASSCharacteristic->getUUID())) {
-		((String)(char*)pCharactersitic->getData()).toCharArray(pass, 20, 0);
+		//((String)(char*)pCharactersitic->getData()).toCharArray(pass, 20, 0);
 	}
+	*/
 }
